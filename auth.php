260d8823fbc8173a54098dfc894a6ffc6f0b497f
Michael <mihailonufriev@gmail.com>
2019-12-19 16:39:59 +0200

    add auth

--- auth.php
+++ auth.php
@@ -1,7 +1,7 @@
 <?php
 $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
-$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+
 if (mb_strlen($login) < 3 || mb_strlen($login) > 90) {
     echo "Length login less 3 symbol";
     exit();
@@ -10,18 +10,22 @@ if (mb_strlen($password) < 6 || mb_strlen($password) > 90) {
     echo "Length login less 6 symbol";
     exit();
 }
-if (mb_strlen($name) < 3 || mb_strlen($name) > 90) {
-    echo "Length login less 3 symbol";
-    exit();
-}
 $password = md5([$password]['salt2']);
 //-------------------------Connection-BD-----------------------------------//
-//$userBD = 'root';
-//$passwordBD = 'password';
-//$mysql = new PDO('mysql:host=localhost;dbname=register-bd', $userBD, $passwordBD);
+$userBD = 'root';
+$passwordBD = 'password';
 $mysql = new mysqli('localhost', 'root', 'password', 'register-bd');
-$mysql->query("INSERT INTO `users` (`login`,`password`,`name`) VALUES ('$login','$password','$name')");
+$result= $mysql->query("SELECT * FROM `users` WHERE `login`='$login'");
+$user = $result->fetch_assoc();
+    if (count($user)==0){
+        echo "Такой пользователь не найден";
+        exit();
+    }
+print_r($user);
+exit();
+
 $mysql->close();
 //------------------------------------------------------------------------//
 header('Location: /');
+
 ?>
